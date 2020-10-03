6f6f377b89d078dbcd15c95f1c7fe1a33d52e57a
Hercules Merscher <hlmerscher@gmail.com>
2020-10-03 11:02:15 +0200

    shortest path algorithm using weighted graph and dijkstra's search

--- algo/graph.go
+++ algo/graph.go
@@ -2,6 +2,8 @@ package algo
 
 import (
 	"bytes"
+	"fmt"
+	"math"
 )
 
 type undirectedGraph struct {
@@ -9,8 +11,17 @@ type undirectedGraph struct {
 	adjList  map[byte][]byte
 }
 
+type weightedGraph struct {
+	vertices []byte
+	adjList  map[byte]map[byte]int
+}
+
 func NewGraph() *undirectedGraph {
-	return &undirectedGraph{}
+	return new(undirectedGraph)
+}
+
+func NewWeightedGraph() *weightedGraph {
+	return new(weightedGraph)
 }
 
 func (g *undirectedGraph) AddVertex(vertex byte) {
@@ -21,9 +32,24 @@ func (g *undirectedGraph) AddVertex(vertex byte) {
 	g.adjList[vertex] = make([]byte, 0)
 }
 
+func (g *weightedGraph) AddVertex(vertex byte) {
+	g.vertices = append(g.vertices, vertex)
+	if g.adjList == nil {
+		g.adjList = make(map[byte]map[byte]int)
+	}
+}
+
 func (g *undirectedGraph) AddEdge(v byte, w byte) {
 	g.adjList[w] = append(g.adjList[w], v)
 	g.adjList[v] = append(g.adjList[v], w)
+}
+
+func (g *weightedGraph) AddEdge(vertex byte, edge byte, weight int) {
+	if g.adjList[vertex] == nil {
+		g.adjList[vertex] = make(map[byte]int)
+	}
+	g.adjList[vertex][edge] = weight
+
 }
 
 func (g *undirectedGraph) String() string {
@@ -34,6 +60,20 @@ func (g *undirectedGraph) String() string {
 
 		for _, n := range g.adjList[v] {
 			b.WriteString(string(n) + " ")
+		}
+
+		b.WriteString("\n")
+	}
+
+	return b.String()
+}
+
+func (g *weightedGraph) String() string {
+	var b bytes.Buffer
+
+	for _, vertex := range g.vertices {
+		for adjVertex, weight := range g.adjList[vertex] {
+			b.WriteString(string(adjVertex) + " -> " + fmt.Sprint(weight) + " -> ")
 		}
 
 		b.WriteString("\n")
@@ -79,4 +119,57 @@ func (g *undirectedGraph) ShortestPath(a byte, b byte) []byte {
 	}
 
 	return reversedPath
+}
+
+func (g *weightedGraph) findLowestCostVertex(costs map[byte]int, processed map[byte]bool) (lowestCostVertex byte) {
+	lowestCost := math.MaxInt64
+
+	for vertex, weight := range costs {
+		if weight < lowestCost && !processed[vertex] {
+			lowestCost = weight
+			lowestCostVertex = vertex
+		}
+	}
+
+	return lowestCostVertex
+}
+
+// Dijkstra returns a path with the minimum number of edges using Dijkstra's search.
+func (g *weightedGraph) Dijkstra(a byte, b byte) ([]byte, int) {
+	costs, parents := make(map[byte]int), make(map[byte]byte)
+
+	for neighbor, weight := range g.adjList[a] {
+		costs[neighbor] = weight
+		parents[neighbor] = a
+	}
+	if _, ok := costs[b]; !ok {
+		costs[b] = math.MaxInt64
+	}
+
+	processed := make(map[byte]bool)
+	node := g.findLowestCostVertex(costs, processed)
+	for node != 0 {
+		cost := costs[node]
+		for neighbor, weight := range g.adjList[node] {
+			newCost := cost + weight
+			if costs[neighbor] > newCost {
+				costs[neighbor] = newCost
+				parents[neighbor] = node
+			}
+		}
+		processed[node] = true
+		node = g.findLowestCostVertex(costs, processed)
+	}
+
+	path, to := make([]byte, 0), b
+	for to != 0 {
+		path = append(path, to)
+		to = parents[to]
+	}
+	reversedPath := make([]byte, len(path))
+	for i, v := range path {
+		reversedPath[len(path)-i-1] = v
+	}
+
+	return reversedPath, costs[b]
 }
--- algo/graph_test.go
+++ algo/graph_test.go
@@ -46,3 +46,44 @@ func TestGraphShortestPath(t *testing.T) {
 		})
 	}
 }
+
+func TestGraphDijkstra(t *testing.T) {
+	graph := NewWeightedGraph()
+	vertices := []byte{'A', 'B', 'C', 'D'}
+	for _, v := range vertices {
+		graph.AddVertex(v)
+	}
+	graph.AddEdge('A', 'B', 2)
+	graph.AddEdge('A', 'C', 6)
+	graph.AddEdge('B', 'C', 3)
+	graph.AddEdge('C', 'D', 1)
+	graph.AddEdge('B', 'D', 5)
+
+	tests := []struct {
+		from         byte
+		to           byte
+		expectedPath []byte
+		distance     int
+	}{
+		{from: 'A', to: 'B', expectedPath: []byte{'A', 'B'}, distance: 2},
+		{from: 'A', to: 'C', expectedPath: []byte{'A', 'B', 'C'}, distance: 5},
+		{from: 'B', to: 'C', expectedPath: []byte{'B', 'C'}, distance: 3},
+		{from: 'A', to: 'D', expectedPath: []byte{'A', 'B', 'C', 'D'}, distance: 6},
+	}
+	for _, s := range tests {
+		t.Run(fmt.Sprintf("From %c to %c", s.from, s.to), func(t *testing.T) {
+			trail, distance := graph.Dijkstra(s.from, s.to)
+			if len(trail) != len(s.expectedPath) {
+				t.Errorf("Path should contain %d vertices", len(s.expectedPath))
+			}
+			if distance != s.distance {
+				t.Errorf("Distance should be %d but returned %d", s.distance, distance)
+			}
+			for i, v := range trail {
+				if v != s.expectedPath[i] {
+					t.Errorf("Vertex at position %d should be %c but returned %c", i, s.expectedPath[i], v)
+				}
+			}
+		})
+	}
+}
